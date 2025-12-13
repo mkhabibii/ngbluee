0f9fe42d0f49d58d051130a35df99d6da8376846
habiboy7 <habiboy0607@gmail.com>
2025-12-13 13:55:05 +0700

    edit home.vue, deleted tailwind.config, modifed main.css & vite.config

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue'
-import { Dialog, DialogPanel } from '@headlessui/vue'
+import { Dialog, DialogPanel, TransitionRoot, TransitionChild } from '@headlessui/vue'
 import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
 
 const mobileMenuOpen = ref(false)
-const isScrolled = ref(false) 
+const isScrolled = ref(false)
 
 const handleScroll = () => {
   isScrolled.value = window.scrollY > 20
@@ -24,43 +24,36 @@ onUnmounted(() => {
     :class="[
       'sticky top-0 z-50 transition-all duration-300',
       isScrolled
-        ? 'bg-black/50 border-white/5 backdrop-blur border-b backdrop-filter ' 
-        : 'bg-black', 
+        ? 'border-white/5 backdrop-blur border-b backdrop-filter bg-black/20 '
+        : 'bg-transparent',
     ]"
   >
     <nav
-      class="mx-auto flex max-w-5xl items-center justify-between p-5 lg:px-8"
+      class="mx-auto flex max-w-6xl items-center justify-between p-4 lg:px-2"
       aria-label="Global"
     >
       <!-- Brand Name -->
-      <div class="flex lg:flex-1">
-        <a href="#" class="text-xl font-bold tracking-widest text-white">HABIBOY7</a>
+      <div class="flex lg:flex-1 font-sofia">
+        <a href="#" class="text-3xl font-extrabold leading-[0.75] tracking-tight">
+          <span class="block">MKH.</span>
+          <span class="block">HABIBI</span>
+        </a>
       </div>
 
       <!-- Desktop Menu -->
-      <div class="hidden lg:flex lg:gap-x-8">
-        <a href="#" class="text-lg font-medium text-gray-300 hover:text-white transition"
-          >Essence</a
-        >
-        <a href="#" class="text-lg font-medium text-gray-300 hover:text-white transition"
-          >Toolkit</a
-        >
-        <a href="#" class="text-lg font-medium text-gray-300 hover:text-white transition"
-          >Showcase</a
-        >
-        <a href="#" class="text-lg font-medium text-gray-300 hover:text-white transition"
-          >Exhibit</a
-        >
-        <a href="#" class="text-lg font-medium text-gray-300 hover:text-white transition"
-          >Credentials</a
-        >
+      <div class="hidden lg:flex lg:gap-x-12 font-manrope">
+        <a href="#" class="text-lg font-semibold hover:text-gray-600 transition">Essence</a>
+        <a href="#" class="text-lg font-semibold hover:text-gray-600 transition">Toolkit</a>
+        <a href="#" class="text-lg font-semibold hover:text-gray-600 transition">Showcase</a>
+        <a href="#" class="text-lg font-semibold hover:text-gray-600 transition">Exhibit</a>
+        <a href="#" class="text-lg font-semibold hover:text-gray-600 transition">Credentials</a>
       </div>
 
       <!-- Right Side -->
-      <div class="hidden lg:flex lg:flex-1 lg:justify-end lg:items-center lg:gap-x-4">
+      <div class="hidden lg:flex lg:flex-1 lg:justify-end lg:items-center lg:gap-x-4 font-manrope">
         <a
           href="#"
-          class="text-sm font-semibold text-white px-6 py-2 rounded-full border-1 border-white hover:bg-white hover:text-black transition"
+          class="text-md font-bold px-6 py-2 rounded-full border-1 border hover:bg-black hover:text-white transition"
         >
           Get In Touch
         </a>
@@ -70,7 +63,7 @@ onUnmounted(() => {
       <div class="flex lg:hidden">
         <button
           type="button"
-          class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
+          class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-800"
           @click="mobileMenuOpen = true"
         >
           <span class="sr-only">Open main menu</span>
@@ -80,64 +73,93 @@ onUnmounted(() => {
     </nav>
 
     <!-- Mobile Menu Dialog -->
-    <Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
-      <div class="fixed inset-0 z-50 bg-black/50"></div>
-      <DialogPanel
-        class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-black p-6 sm:max-w-sm sm:ring-1 sm:ring-white/10"
-      >
-        <div class="flex items-center justify-between">
-          <a href="#" class="-m-1.5 p-1.5">
-            <span class="text-xl font-bold tracking-widest text-white">HABIBOY7</span>
-          </a>
-          <button
-            type="button"
-            class="-m-2.5 rounded-md p-2.5 text-gray-400 hover:text-white"
-            @click="mobileMenuOpen = false"
-          >
-            <span class="sr-only">Close menu</span>
-            <XMarkIcon class="size-6" aria-hidden="true" />
-          </button>
-        </div>
+    <TransitionRoot :show="mobileMenuOpen" as="template">
+      <Dialog class="lg:hidden" @close="mobileMenuOpen = false">
+        <!-- Backdrop -->
+        <TransitionChild
+          as="template"
+          enter="ease-out duration-300"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="ease-in duration-200"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
+          <div class="fixed inset-0 z-50 bg-black/30" aria-hidden="true"></div>
+        </TransitionChild>
 
-        <div class="mt-6 flow-root">
-          <div class="-my-6 divide-y divide-white/10">
-            <div class="space-y-2 py-6">
+        <!-- Panel Slider -->
+        <TransitionChild
+          as="template"
+          enter="transform transition ease-out duration-300"
+          enter-from="translate-x-full"
+          enter-to="translate-x-0"
+          leave="transform transition ease-in duration-200"
+          leave-from="translate-x-0"
+          leave-to="translate-x-full"
+        >
+          <DialogPanel
+            class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-black/20 backdrop-blur-md p-6 sm:max-w-sm sm:ring-1 sm:ring-white/10"
+          >
+            <div class="flex items-center justify-between">
               <a
                 href="#"
-                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-medium text-gray-300 hover:bg-white/5 hover:text-white"
-                >Essence</a
+                class="font-sofia text-black hover:text-gray-300 -m-1.5 p-1.5 text-2xl font-extrabold leading-[0.75] tracking-tight"
               >
-              <a
-                href="#"
-                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-medium text-gray-300 hover:bg-white/5 hover:text-white"
-                >Toolkit</a
+                <span class="block">MKH.</span>
+                <span class="block">HABIBI</span>
+              </a>
+              <button
+                type="button"
+                class="-m-2.5 rounded-md p-2.5 text-black-800 hover:text-gray-600"
+                @click="mobileMenuOpen = false"
               >
-              <a
-                href="#"
-                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-medium text-gray-300 hover:bg-white/5 hover:text-white"
-                >Showcase</a
-              >
-              <a
-                href="#"
-                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-medium text-gray-300 hover:bg-white/5 hover:text-white"
-                >Exhibit</a
-              >
-              <a
-                href="#"
-                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-medium text-gray-300 hover:bg-white/5 hover:text-white"
-                >Credentials</a
-              >
+                <span class="sr-only">Close menu</span>
+                <XMarkIcon class="size-6" aria-hidden="true" />
+              </button>
             </div>
-            <div class="py-6 space-y-4">
-              <a
-                href="#"
-                class="-mx-3 block rounded-full px-6 py-3 text-center text-base/7 font-semibold text-white border-2 border-white hover:bg-white hover:text-black transition"
-                >Get in Touch</a
-              >
+
+            <div class="mt-6 flow-root">
+              <div class="-my-6 divide-y divide-white/10 font-manrope">
+                <div class="space-y-2 py-6">
+                  <a
+                    href="#"
+                    class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-medium hover:bg-black/10 hover:text-white"
+                    >Essence</a
+                  >
+                  <a
+                    href="#"
+                    class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-medium hover:bg-black/10 hover:text-white"
+                    >Toolkit</a
+                  >
+                  <a
+                    href="#"
+                    class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-medium hover:bg-black/10 hover:text-white"
+                    >Showcase</a
+                  >
+                  <a
+                    href="#"
+                    class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-medium hover:bg-black/10 hover:text-white"
+                    >Exhibit</a
+                  >
+                  <a
+                    href="#"
+                    class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-medium hover:bg-black/10 hover:text-white"
+                    >Credentials</a
+                  >
+                </div>
+                <div class="py-6 space-y-4">
+                  <a
+                    href="#"
+                    class="-mx-3 block rounded-full px-6 py-3 text-center text-base/7 font-semibold border-2 border-black hover:bg-black hover:text-white transition"
+                    >Get in Touch</a
+                  >
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </DialogPanel>
-    </Dialog>
+          </DialogPanel>
+        </TransitionChild>
+      </Dialog>
+    </TransitionRoot>
   </header>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,42 @@
 <template>
-  <p>ini home</p>
+  <section class="relative min-h-screen overflow-hidden flex">
+    <div class="w-full max-w-7xl mx-auto px-6 lg:px-12 relative">
+      <!-- HERO TITLE  -->
+      <div class="absolute top-12 left-1/2 -translate-x-1/2 z-10 whitespace-nowrap">
+        <span class="block text-xl lg:text-4xl font-light mb-[-0.1em]"> Enthusiast </span>
+
+        <h1
+          class="font-sofia text-[12vw] lg:text-[14rem] font-extrabold leading-[0.75] tracking-[-0.08em]"
+        >
+          WEB&nbsp;DEVELOPER
+        </h1>
+
+        <span
+          class="absolute right-0 text-sm lg:text-xl font-light leading-[0.5] tracking-[0.3em] font-semibold"
+          >BASED IN YOGYAKARTA</span
+        >
+      </div>
+
+      <!-- CONTENT CENTER GROUP -->
+      <div class="relative mt-[10rem] sm:mt-[18rem] lg:mt-[12rem] flex justify-center">
+        <div class="flex flex-col lg:flex-row items-end gap-4 lg:gap-8">
+          <!-- TEXT -->
+          <div class="font-sofia font-bold text-2xl tracking-[-0.05em] text-center lg:text-left">
+            <div>/ WEB DEVELOPMENT</div>
+            <div>/ WEB DESIGN (UX/UI)</div>
+            <div>/ SENENG NGFOTO</div>
+          </div>
+
+          <!-- IMAGE -->
+          <div class="w-45 sm:w-72 lg:w-80 aspect-[3/4] overflow-hidden">
+            <img
+              src="/src/assets/img/nuba.jpeg"
+              class="w-full h-full object-cover grayscale"
+              alt="Habibi"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
